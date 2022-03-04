11421e096a67f3907abfd10ab69b490df199579e
Sava Gaborov <savagaborov@gmail.com>
2022-03-04 12:03:28 +0100

    adjust pom xml and add websocket configuration

---
+++ src/main/java/store/configuration/WebSocketConfiguration.java
@@ -0,0 +1,25 @@
+package store.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+                                    //TODO depricated version but with the newest don't work
+
+        @Override
+        public void configureMessageBroker(MessageBrokerRegistry config) {
+            config.enableSimpleBroker("/broker");
+            config.setApplicationDestinationPrefixes("/sockets");
+        }
+
+        @Override
+        public void registerStompEndpoints(StompEndpointRegistry registry) {
+            registry.addEndpoint("/session");
+            registry.addEndpoint("/session").withSockJS();
+        }
+}
